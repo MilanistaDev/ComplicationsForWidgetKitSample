1594574f74d14deca4d69c2079243d4021440420
Takuya Aso <milanista224@gmail.com>
2023-12-06 17:01:45 +0900

    Initial Commit

---
+++ ComplicationsForWidgetKitSample Watch App/ComplicationsForWidgetKitSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComplicationsForWidgetKitSampleApp.swift
+//  ComplicationsForWidgetKitSample Watch App
+//
+//  Created by Takuya Aso on 2023/12/06.
+//
+
+import SwiftUI
+
+@main
+struct ComplicationsForWidgetKitSample_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ComplicationsForWidgetKitSample Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ComplicationsForWidgetKitSample Watch App
+//
+//  Created by Takuya Aso on 2023/12/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
