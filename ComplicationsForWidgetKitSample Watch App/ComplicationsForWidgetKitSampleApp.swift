7668ccde13b7d272398d9681a0a97dbbcd88fa62
Takuya Aso <milanista224@gmail.com>
2023-12-06 18:26:02 +0900

    Implementation of TopContentView

--- ComplicationsForWidgetKitSample Watch App/ComplicationsForWidgetKitSampleApp.swift
+++ ComplicationsForWidgetKitSample Watch App/ComplicationsForWidgetKitSampleApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ComplicationsForWidgetKitSample_Watch_AppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TopContentView()
         }
     }
 }
---
+++ ComplicationsForWidgetKitSample Watch App/TopContentView.swift
@@ -0,0 +1,41 @@
+//
+//  TopContentView.swift
+//  ComplicationsForWidgetKitSample Watch App
+//
+//  Created by Takuya Aso on 2023/12/06.
+//
+
+import SwiftUI
+
+struct TopContentView: View {
+    @State private var selectedContent: AppContent?
+
+    var body: some View {
+        NavigationSplitView {
+            List(selection: $selectedContent) {
+                ForEach(topContents, id: \.self) { content in
+                    TopContentRow(content: content)
+                }
+            }
+            .listStyle(CarouselListStyle())
+            .navigationTitle("Sample App")
+            .navigationBarTitleDisplayMode(.large)
+            .containerBackground(.cyan.opacity(0.5).gradient, for: .navigation)
+        } detail: {
+            switch selectedContent?.type {
+            case .main:
+                Text("Main")
+
+            case .menu:
+                Text("Menu")
+
+            case nil:
+                EmptyView()
+            }
+        }
+    }
+}
+
+#Preview {
+    TopContentView()
+}
