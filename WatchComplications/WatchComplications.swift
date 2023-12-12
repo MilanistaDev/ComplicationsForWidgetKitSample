cb6547fbfb4d2d70c32cb5166cb4a311a64adc5f
Takuya Aso <milanista224@gmail.com>
2023-12-12 23:54:34 +0900

    #3 Implementation for Widget UI and Time Line.

--- WatchComplications/WatchComplications.swift
+++ WatchComplications/WatchComplications.swift
@@ -10,48 +10,33 @@ import SwiftUI
 
 struct Provider: TimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), emoji: "😀")
+        SimpleEntry(date: Date())
     }
 
     func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), emoji: "😀")
+        let entry = SimpleEntry(date: Date())
         completion(entry)
     }
 
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, emoji: "😀")
-            entries.append(entry)
-        }
-
-        let timeline = Timeline(entries: entries, policy: .atEnd)
+        let entries: [SimpleEntry] = [SimpleEntry(date: Date())]
+        // No need update timeline
+        let timeline = Timeline(entries: entries, policy: .never)
         completion(timeline)
     }
 }
 
 struct SimpleEntry: TimelineEntry {
     let date: Date
-    let emoji: String
 }
 
 struct WatchComplicationsEntryView : View {
     var entry: Provider.Entry
 
     var body: some View {
-        VStack {
-            HStack {
-                Text("Time:")
-                Text(entry.date, style: .time)
-            }
-
-            Text("Emoji:")
-            Text(entry.emoji)
-        }
+        Image("WatchComplicationIcon")
+            .resizable()
+            .scaledToFit()
     }
 }
 
@@ -70,14 +55,14 @@ struct WatchComplications: Widget {
                     .background()
             }
         }
-        .configurationDisplayName("My Widget")
+        .configurationDisplayName("Complication sample")
         .description("This is an example widget.")
+        .supportedFamilies([.accessoryCircular]) // ひとつのみ
     }
 }
 
 #Preview(as: .accessoryRectangular) {
     WatchComplications()
 } timeline: {
-    SimpleEntry(date: .now, emoji: "😀")
-    SimpleEntry(date: .now, emoji: "🤩")
+    SimpleEntry(date: .now)
 }
